d59c33f139c88e87919cbc1930f2694412288f74
Skylar Grant <official.xian@gmail.com>
2021-12-10 15:17:22 -0500

    Concept code

---
+++ main.js
@@ -0,0 +1,27 @@
+// node library pigpio is a wrapper for the pigpio C library.
+const Gpio = require('pigpio').Gpio;
+
+// Import the config file
+const config = require('./config.json');
+const pins = config.pins;
+
+// Set up the switches
+const switchOne = new Gpio(pins.s1, {
+	mode: Gpio.INPUT,
+	pullUpDown: Gpio.PUD_DOWN,
+	edge: Gpio.EITHER_EDGE
+});
+
+switchOne.on('interrupt', (level) => {
+	switch (level) {
+		case 0:
+			// Off
+			break;
+		case 1:
+			// On
+			break;
+		default:
+			// Timeout (2)
+			break;
+	}
+});
