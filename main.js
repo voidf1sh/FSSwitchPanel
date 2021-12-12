49e5068ab9b6a030d2d5a03a0605a2a697c7712f
Skylar Grant <official.xian@gmail.com>
2021-12-12 14:52:24 -0500

    Add buttons

--- main.js
+++ main.js
@@ -57,6 +57,60 @@ const s09 = new Gpio(pins.s09, {
 	edge: Gpio.RISING_EDGE
 });
 
+// Set up Buttons
+const b00 = new Gpio(pins.b00, {
+	mode: Gpio.INPUT,
+	pullUpDown: Gpio.PUD_DOWN,
+	edge: Gpio.RISING_EDGE
+});
+const b01 = new Gpio(pins.b01, {
+	mode: Gpio.INPUT,
+	pullUpDown: Gpio.PUD_DOWN,
+	edge: Gpio.RISING_EDGE
+});
+const b02 = new Gpio(pins.b02, {
+	mode: Gpio.INPUT,
+	pullUpDown: Gpio.PUD_DOWN,
+	edge: Gpio.RISING_EDGE
+});
+const b03 = new Gpio(pins.b03, {
+	mode: Gpio.INPUT,
+	pullUpDown: Gpio.PUD_DOWN,
+	edge: Gpio.RISING_EDGE
+});
+const b04 = new Gpio(pins.b04, {
+	mode: Gpio.INPUT,
+	pullUpDown: Gpio.PUD_DOWN,
+	edge: Gpio.RISING_EDGE
+});
+const b05 = new Gpio(pins.b05, {
+	mode: Gpio.INPUT,
+	pullUpDown: Gpio.PUD_DOWN,
+	edge: Gpio.RISING_EDGE
+});
+const b06 = new Gpio(pins.b06, {
+	mode: Gpio.INPUT,
+	pullUpDown: Gpio.PUD_DOWN,
+	edge: Gpio.RISING_EDGE
+});
+const b07 = new Gpio(pins.b07, {
+	mode: Gpio.INPUT,
+	pullUpDown: Gpio.PUD_DOWN,
+	edge: Gpio.RISING_EDGE
+});
+const b08 = new Gpio(pins.b08, {
+	mode: Gpio.INPUT,
+	pullUpDown: Gpio.PUD_DOWN,
+	edge: Gpio.RISING_EDGE
+});
+const b09 = new Gpio(pins.b09, {
+	mode: Gpio.INPUT,
+	pullUpDown: Gpio.PUD_DOWN,
+	edge: Gpio.RISING_EDGE
+});
+
+
+// Handle Switch input
 s00.on('interrupt', (level) => {
 	switch (level) {
 		case 0:
@@ -214,6 +268,168 @@ s09.on('interrupt', (level) => {
 		default:
 			// Timeout (2)
 			console.log('Switch 09 Timed Out');
+			break;
+	}
+});
+
+// Handle Button Input
+b00.on('interrupt', (level) => {
+	switch (level) {
+		case 0:
+			// Off
+			console.log('Button 00 Off');
+			break;
+		case 1:
+			// On
+			console.log('Button 00 On');
+			break;
+		default:
+			// Timeout (2)
+			console.log('Button 00 Timed Out');
+			break;
+	}
+});
+b01.on('interrupt', (level) => {
+	switch (level) {
+		case 0:
+			// Off
+			console.log('Button 01 Off');
+			break;
+		case 1:
+			// On
+			console.log('Button 01 On');
+			break;
+		default:
+			// Timeout (2)
+			console.log('Button 01 Timed Out');
+			break;
+	}
+});
+b02.on('interrupt', (level) => {
+	switch (level) {
+		case 0:
+			// Off
+			console.log('Button 02 Off');
+			break;
+		case 1:
+			// On
+			console.log('Button 02 On');
+			break;
+		default:
+			// Timeout (2)
+			console.log('Button 02 Timed Out');
+			break;
+	}
+});
+b03.on('interrupt', (level) => {
+	switch (level) {
+		case 0:
+			// Off
+			console.log('Button 03 Off');
+			break;
+		case 1:
+			// On
+			console.log('Button 03 On');
+			break;
+		default:
+			// Timeout (2)
+			console.log('Button 03 Timed Out');
+			break;
+	}
+});
+b04.on('interrupt', (level) => {
+	switch (level) {
+		case 0:
+			// Off
+			console.log('Button 04 Off');
+			break;
+		case 1:
+			// On
+			console.log('Button 04 On');
+			break;
+		default:
+			// Timeout (2)
+			console.log('Button 04 Timed Out');
+			break;
+	}
+});
+b05.on('interrupt', (level) => {
+	switch (level) {
+		case 0:
+			// Off
+			console.log('Button 05 Off');
+			break;
+		case 1:
+			// On
+			console.log('Button 05 On');
+			break;
+		default:
+			// Timeout (2)
+			console.log('Button 05 Timed Out');
+			break;
+	}
+});
+b06.on('interrupt', (level) => {
+	switch (level) {
+		case 0:
+			// Off
+			console.log('Button 06 Off');
+			break;
+		case 1:
+			// On
+			console.log('Button 06 On');
+			break;
+		default:
+			// Timeout (2)
+			console.log('Button 06 Timed Out');
+			break;
+	}
+});
+b07.on('interrupt', (level) => {
+	switch (level) {
+		case 0:
+			// Off
+			console.log('Button 07 Off');
+			break;
+		case 1:
+			// On
+			console.log('Button 07 On');
+			break;
+		default:
+			// Timeout (2)
+			console.log('Button 07 Timed Out');
+			break;
+	}
+});
+b08.on('interrupt', (level) => {
+	switch (level) {
+		case 0:
+			// Off
+			console.log('Button 08 Off');
+			break;
+		case 1:
+			// On
+			console.log('Button 08 On');
+			break;
+		default:
+			// Timeout (2)
+			console.log('Button 08 Timed Out');
+			break;
+	}
+});
+b09.on('interrupt', (level) => {
+	switch (level) {
+		case 0:
+			// Off
+			console.log('Button 09 Off');
+			break;
+		case 1:
+			// On
+			console.log('Button 09 On');
+			break;
+		default:
+			// Timeout (2)
+			console.log('Button 09 Timed Out');
 			break;
 	}
 });
